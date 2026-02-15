27c66d902d3042d8399fc95f4d6392483e63af37
João Bruno <joaobrunoah@gmail.com>
2026-02-15 19:06:06 -0300

    Add cerimony details

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -27,8 +27,30 @@ const Home: React.FC = () => {
         </div>
       </section>
       <section id="ceremony">
-        <h1>Cerimônia e Festa</h1>
-        {/* Content will be added later */}
+        <div className="ceremony-container">
+          <div className="ceremony-image-column">
+            <img src="/assets/villa_mandacaru.jpeg" alt="Villa Mandacarú" />
+          </div>
+          <div className="ceremony-text-column">
+            <h1>Cerimônia e Festa</h1>
+            <p className="ceremony-text">
+              Um fim de tarde gostoso, lugar incrível e todas as pessoas que a gente ama reunidas.<br/>
+
+              É exatamente esse clima que queremos viver com vocês!<br/>
+
+              No dia <b>13 de junho de 2026</b>, na Villa Mandacarú, em Itu, vamos celebrar nosso amor. Às 15h tudo estará pronto para receber vocês, podem chegar tranquilos e desfrutar de toda beleza do local. <br/>
+              
+              Após a cerimônia vamos receber todos para a festa que estamos preparando cada detalhe com muito amor.<br/>
+              
+              Tudo no mesmo cenário, sem pressa, só curtindo cada momento.<br/>
+              
+              <b>Do pôr do sol à pista de dança</b><br/>
+            </p>
+            <a href="https://maps.app.goo.gl/FYJqsyqqtxUFCwt28" target="_blank" rel="noopener noreferrer" className="ceremony-map-link">
+              Acesse o mapa para a Villa
+            </a>
+          </div>
+        </div>
       </section>
       <section id="hotels">
         <h1>Hotéis</h1>
