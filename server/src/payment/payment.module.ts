85cbdbd8c339175ec779328afcac379bdf092550
João Bruno <joaobrunoah@gmail.com>
2026-02-17 20:41:43 -0300

    Fix server

--- server/src/payment/payment.module.ts
+++ server/src/payment/payment.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PaymentController } from './payment.controller';
 import { PaymentService } from './payment.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PaymentController],
   providers: [PaymentService],
 })
