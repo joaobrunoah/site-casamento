97b0ec5f88fadef3ff021d4a746b1270d1ee4d12
João Bruno <joaobrunoah@gmail.com>
2026-02-19 20:28:59 -0300

    Remove dist files

--- server/dist/auth/auth.service.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class AuthService {
-    private readonly adminHash;
-    constructor();
-    generateAdminHash(): string;
-    validateHash(authHash: string): boolean;
-    validateCredentials(user: string, password: string): boolean;
-}
